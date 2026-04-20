85d4f0a708a03b3762e781c83a5221786e8d984c
Peter Son <peterson@Peters-MacBook-Air.local>
2026-04-20 10:40:12 -0700

    initial commit

---
+++ xestohandtest/xestohandtest/xestohandtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  xestohandtestApp.swift
+//  xestohandtest
+//
+//  Created by Peter Son on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct xestohandtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xestohandtest/xestohandtestTests/xestohandtestTests.swift
@@ -0,0 +1,17 @@
+//
+//  xestohandtestTests.swift
+//  xestohandtestTests
+//
+//  Created by Peter Son on 2026-04-20.
+//
+
+import Testing
+@testable import xestohandtest
+
+struct xestohandtestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
